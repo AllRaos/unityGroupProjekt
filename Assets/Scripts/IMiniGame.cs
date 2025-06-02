c44b62c5b215c9ee3197b604d0d530e99058a7f3
Vasya <114648151+VasylParaniuk@users.noreply.github.com>
2025-06-02 10:48:44 +0300

    Wtih Biuld Project

--- Assets/Scripts/IMiniGame.cs
+++ Assets/Scripts/IMiniGame.cs
@@ -1,4 +1,3 @@
-using UnityEngine;
 using static PlayerController;
 
 public interface IMiniGame
--- Assets/Scripts/InventoryScripts/Inventory.cs
+++ Assets/Scripts/InventoryScripts/Inventory.cs
@@ -1,6 +1,4 @@
 using System.Collections.Generic;
-using UnityEngine;
-using static UnityEditor.Progress;
 
 [System.Serializable]
 public class Inventory
